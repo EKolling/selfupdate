d32fe25e9d9bfb48d2a061c9db875a26362e43bb
EKolling <82099922+EKolling@users.noreply.github.com>
2023-05-04 13:29:33 -0300

    Update comparaecopia.js

--- comparaecopia.js
+++ comparaecopia.js
@@ -1,68 +1,90 @@
 //compara arquivo local e do servidor, se diferente, salva na pasta Downloads.
 
-const vi = require('win-version-info');
-const ftp = require('basic-ftp');
-const fs = require('fs');
-const path = require('path');
+import vi from "win-version-info";
+import { Client } from "basic-ftp";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
-// Caminho para o programa local
-const programPath = 'C:\\LOCAL\\do\\arquivo\\instalado.exe';
+export default class Copiador {
+  async copiar() {
+    try {
+      // Caminho para o programa local
+      const programPath =
 
-// Lendo o arquivo config.txt
-const configFile = fs.readFileSync('./config.txt', 'utf8');
-const config = JSON.parse(configFile);
+      "C:\\Users\\HELPER\\AppData\\Local\\Programs\\helper-totem\\helper-totem.exe";
 
-// Criando a conexão FTP
-const client = new ftp.Client();
-client.ftp.verbose = true;
+      const currentFilePath = fileURLToPath(import.meta.url);
+      const currentDirPath = dirname(currentFilePath);
+      const configFilePath = join(
+      currentDirPath, "../configuracoes/acessoftp.txt")
 
-(async () => {
-  try {
-    // Estabelecendo a conexão
-    await client.access({
-      host: config.host,
-      user: config.user,
-      password: config.password,
-      secure: false
-    });
+      // Lendo o arquivo config.txt
+      const configFile = readFileSync(configFilePath, "utf8");
 
-    // Obtendo a lista de arquivos na pasta
-    const list = await client.list();
+      const config = JSON.parse(configFile);
 
-    // Procurando o arquivo
-    const file = list.find(f => f.name.includes('BUSCA DE NOME DO ARQUIVO'));
+      // Criando a conexão FTP
+      const client = new Client();
+      client.ftp.verbose = true;
 
-    // Extraindo a versão do arquivo no servidor
-    const serverVersion = file.name.match(/Setup (\d+\.\d+\.\d+)\.exe/)[1];
+      // Estabelecendo a conexão
+      //await new Promise((resolve, reject) => {
+      
+        await client.access({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        secure: false,
+      });
 
-    // Obtendo a versão do programa local
-    const info = vi(programPath);
-    const localVersion = info.FileVersion;
 
-    console.log(`Versão local do programa: ${localVersion}`);
-    console.log(`Versão do arquivo no servidor: ${serverVersion}`);
+      // Obtendo a lista de arquivos na pasta
+      const list = await client.list();
 
-    // Verificando se a versão do arquivo no servidor é mais recente do que a local
-    if (serverVersion > localVersion) {
-      console.log('O arquivo no servidor é mais atualizado. Baixando...');
+      // Procurando o arquivo
+      const file = list.find((f) => f.name.includes("helper-totem Setup"));
 
-      // Criando o caminho completo para a pasta "Downloads"
-      const downloadsDir = path.join(process.env.USERPROFILE, 'Downloads');
+      // Extraindo a versão do arquivo no servidor
+      const serverVersion = file.name.match(
+        /helper-totem Setup (\d+\.\d+\.\d+)\.exe/
+      )[1];
 
-      // Criando o caminho completo para o arquivo local na pasta "Downloads"
-      const localFilePath = path.join(downloadsDir, file.name);
+      // Obtendo a versão do programa local
+      const info = vi(programPath);
+      const localVersion = info.FileVersion;
 
-      // Baixando o arquivo para a pasta "Downloads"
-      await client.downloadTo(localFilePath, file.name);
+      console.log(`Versão local do programa: ${localVersion}`);
+      console.log(`Versão do arquivo no servidor: ${serverVersion}`);
 
-      console.log('Arquivo baixado com sucesso!');
-    } else {
-      console.log('A versão do arquivo no servidor é igual ou anterior à versão local. Não é necessário fazer o download.');
+      // Verificando se a versão do arquivo no servidor é mais recente do que a local
+      if (serverVersion > localVersion) {
+        console.log("O arquivo no servidor é mais atualizado. Baixando...");
+
+        // Criando o caminho completo para a pasta "Downloads"
+        const downloadsDir = join(process.env.USERPROFILE, "Downloads");
+
+        // Criando o caminho completo para o arquivo local na pasta "Downloads"
+        const localFilePath = join(downloadsDir, file.name);
+
+        // Baixando o arquivo para a pasta "Downloads"
+        await client.downloadTo(localFilePath, file.name);
+       
+
+        console.log("Arquivo baixado com sucesso!");
+
+        return true
+
+      } else {
+        console.log(
+          "A versão do arquivo no servidor é igual ou anterior à versão local. Não é necessário fazer o download."
+        );
+      }
+
+      // Encerrando a conexão
+      await client.close();
+    } catch (err) {
+      console.error(err);
     }
-
-    // Encerrando a conexão
-    await client.close();
-  } catch(err) {
-    console.error(err);
   }
-})();
+}
