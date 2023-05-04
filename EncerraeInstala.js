2f5f2a5a8dbe1d5881c131705cdcfb8b8b089c36
EKolling <82099922+EKolling@users.noreply.github.com>
2023-05-04 13:29:50 -0300

    Update EncerraeInstala.js

--- EncerraeInstala.js
+++ EncerraeInstala.js
@@ -1,44 +1,61 @@
-const fs = require('fs');
-const path = require('path');
-const { exec } = require('child_process');
+import { readdirSync } from "fs";
+import { join } from "path";
+import { exec } from "child_process";
 
-// Obter o caminho da pasta Downloads
-const downloadFolder = path.join(process.env.USERPROFILE, 'Downloads');
+export default class Encerrador {
+  //const configFile = './configuracoes/configmonitoramento.txt';
 
-// Encontrar o arquivo do instalador mais recente
-let latestInstaller = '';
-let latestVersion = '';
-fs.readdirSync(downloadFolder).forEach(file => {
-  const match = file.match(/^Setup (\d+\.\d+\.\d+)\.exe$/i);
-  if (match) {
-    const version = match[1];
-    if (version > latestVersion) {
-      latestInstaller = file;
-      latestVersion = version;
-    }
-  }
-});
+  async encerrar() {
+    try {
+      // Obter o caminho da pasta Downloads
+      const downloadFolder = join(process.env.USERPROFILE, "Downloads");
 
-// Verificar se foi encontrado um arquivo do instalador
-if (!latestInstaller) {
-  console.error('Não foi possível encontrar o arquivo do instalador mais recente.');
-  return;
-}
-// Encerra a aplicação antiga, para nao pedir OK durante a instalação.
-exec('taskkill /im programaemprocesso.exe /f', (err, stdout, stderr) => {
-    if (err) {
-      console.error(`Erro ao encerrar o processo: ${err.message}`);
+      // Encontrar o arquivo do instalador mais recente
+      let latestInstaller = "";
+      let latestVersion = "";
+      readdirSync(downloadFolder).forEach((file) => {
+        const match = file.match(/^helper-totem Setup (\d+\.\d+\.\d+)\.exe$/i);
+        if (match) {
+          const version = match[1];
+          if (version > latestVersion) {
+            latestInstaller = file;
+            latestVersion = version;
+          }
+        }
+      });
+
+      // Verificar se foi encontrado um arquivo do instalador
+      if (!latestInstaller) {
+        console.error(
+          "Não foi possível encontrar o arquivo do instalador mais recente."
+        );
+        return;
+      }
+
+      exec("taskkill /im helper-totem.exe /f", (err, stdout, stderr) => {
+        if (err) {
+          console.error(`Erro ao encerrar o processo: ${err.message}`);
+          return;
+        }
+      });
+
+      // Executar o instalador
+      const installerPath = join(downloadFolder, latestInstaller);
+
+      await new Promise((resolve, reject) => {
+        exec(`"${installerPath}"`, (err, stdout, stderr) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(true);
+          }
+          console.log("Instalação concluída com sucesso.");
+        });
+      });
+      return true;
+    } catch (error) {
+      console.error(`Erro ao executar o instalador: ${err.message}`);
       return;
     }
-});
-
-// Executar o instalador
-const installerPath = path.join(downloadFolder, latestInstaller);
-exec(`"${installerPath}"`, (err, stdout, stderr) => {
-  if (err) {
-    console.error(`Erro ao executar o instalador: ${err.message}`);
-    return;
   }
-
-  console.log('Instalação concluída com sucesso.');
-});
+}
