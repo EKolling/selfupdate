c89dd12239559bf3016ff68a50eca8fc947faf8e
EKolling <82099922+EKolling@users.noreply.github.com>
2023-05-04 13:30:24 -0300

    Update reiniciar.js

--- reiniciar.js
+++ reiniciar.js
@@ -1,14 +1,26 @@
-const cron = require('node-cron');
-const exec = require('child_process').exec;
+import cron from "node-cron";
+import exec from "child_process";
 
-// Agendando tarefa para executar às 6h ou 9h das terças-feiras
-cron.schedule('0 6,9 * * 2', () => {
-  console.log('Reiniciando o computador...');
-  exec('shutdown -r -f', (error, stdout, stderr) => {
-    if (error) {
+export default class Reiniciador {
+  //const configFile = './configuracoes/configmonitoramento.txt';
+
+  async reiniciar() {
+    try {
+      //module.exports = async () => {
+      // Agendando tarefa para executar às 6h ou 9h das terças-feiras
+      //cron.schedule('0 6,9 * * 2', () => {
+      console.log("Reiniciando o computador...");
+      exec("shutdown -r -f", (error, stdout, stderr) => {
+        if (error) {
+          throw new Error();
+
+          return;
+        }
+        console.log(`Resultado da reinicialização: ${stdout}`);
+      });
+      //});
+    } catch (error) {
       console.error(`Erro ao reiniciar o computador: ${error}`);
-      return;
     }
-    console.log(`Resultado da reinicialização: ${stdout}`);
-  });
-});
+  }
+}
