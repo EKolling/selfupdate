f7d97d653fd4b3efd4d22f2617add315300079dc
EKolling <82099922+EKolling@users.noreply.github.com>
2023-05-04 13:30:08 -0300

    Update monitoramento.js

--- monitoramento.js
+++ monitoramento.js
@@ -1,62 +1,112 @@
-const fs = require('fs');
-const { exec } = require('child_process');
+import fs from "fs";
+import { exec } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
+//module.exports = async () => {
 // Ler arquivo de configuração
-const configFile = './monitorprocesso.txt';
-let config = {};
+export default class Monitor {
+  //const configFile = './configuracoes/configmonitoramento.txt';
 
-try {
-  const data = fs.readFileSync(configFile, 'utf8');
-  const lines = data.split('\n');
-
-  for (let line of lines) {
-    if (line.trim() !== '') {
-      const [key, value] = line.split('=');
-      if (key && value) {
-        config[key.trim()] = value.trim();
-      }
-    }
+  constructor() {
+    
+    this.monitoramento = true;
   }
-} catch (err) {
-  console.error(`Erro ao ler o arquivo de configuração: ${err}`);
-  process.exit(1);
-}
 
-// Iniciar loop de monitoramento
-let counter = 0;
-const interval = setInterval(() => {
-  for (let key in config) {
-    if (key.startsWith('process')) {
-      const process = config[key];
-      const pathKey = key.replace('process', 'path');
-      const path = config[pathKey];
-      
-      // Verificar se o processo está em execução
-      exec(`tasklist /FI "IMAGENAME eq ${process}.exe"`, (err, stdout) => {
-        if (err) {
-          console.error(`Erro ao executar o comando: ${err}`);
-          return;
-        }
+  async start_monitorar() {
+    //if (this.monitoramento) {
+      //return;
+    //}
 
-        if (stdout.includes(process)) {
-          console.log(`O processo ${process} está em execução.`);
-          counter = 0; // Reiniciar contador
-        } else {
-          console.log(`O processo ${process} não está em execução.`);
-          counter += 10; // Incrementar contador
-          if (counter >= 10) {
-            // Iniciar o processo
-            exec(`start "" "${path}"`, (err) => {
-              if (err) {
-                console.error(`Erro ao executar o comando: ${err}`);
-                return;
-              }
-              console.log(`O processo ${process} foi iniciado.`);
-            });
-            counter = 0; // Reiniciar contador
+    this.monitoramento = true;
+    console.log("Monitoramento ligado");
+    this.monitorar();
+  }
+
+  monitorar() {
+
+    let config = {};
+
+
+    try {
+      const currentFilePath = fileURLToPath(import.meta.url);
+      const currentDirPath = dirname(currentFilePath);
+      const configFilePath = join(
+        currentDirPath,
+        "../configuracoes/configmonitoramento.txt"
+      );
+      const data = fs.readFileSync(configFilePath, "utf8");
+      const lines = data.split("\n");
+
+      for (let line of lines) {
+        if (line.trim() !== "") {
+          const [key, value] = line.split("=");
+          if (key && value) {
+            config[key.trim()] = value.trim();
           }
         }
-      });
+      }
+      // Iniciar loop de monitoramento
+      let counter = 0;
+      const interval = setInterval(() => {
+        if (this.monitoramento == false) {
+          return;
+        }
+        console.log('Monitorando.......')
+        for (let key in config) {
+          if (key.startsWith("process")) {
+            const process = config[key];
+            const pathKey = key.replace("process", "path");
+            const path = config[pathKey];
+
+            // Verificar se o processo está em execução
+            exec(
+              `tasklist /FI "IMAGENAME eq ${process}.exe"`,
+              (err, stdout) => {
+                if (err) {
+                  console.error(`Erro ao executar o comando: ${err}`);
+                  return;
+                }
+
+                if (stdout.includes(process)) {
+                  console.log(`O processo ${process} está em execução.`);
+                  counter = 0; // Reiniciar contador
+                } else {
+                  console.log(`O processo ${process} não está em execução.`);
+                  counter += 10; // Incrementar contador
+                  if (counter >= 10) {
+                    // Iniciar o processo
+                    exec(`start "" "${path}"`, (err) => {
+                      if (err) {
+                        console.error(`Erro ao executar o comando: ${err}`);
+                        return;
+                      }
+                      console.log(`O processo ${process} foi iniciado.`);
+                    });
+                    counter = 0; // Reiniciar contador
+                  }
+                }
+              }
+            );
+          }
+        }
+      }, 30000);
+    } catch (err) {
+      console.error(`Erro ao ler o arquivo de configuração: ${err}`);
+      process.exit(1);
     }
   }
-}, 10000);
+
+  async stop_monitorar() {
+    if (!this.monitoramento) {
+      return;
+    }
+    this.monitoramento = false;
+    console.log("Monitoramento desligado");
+  }
+
+  async restart_monitorar() {
+    this.monitoramento = true;
+    start_monitorar();
+  }
+}
